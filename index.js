6a3c30965c492badffd3fc1374b760c8887ba79d
Dustin Shropshire <dustinshropshire@Dustins-MacBook-Pro.local>
2018-10-18 18:35:25 -0600

    moved to new system

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ let webServer = require("./components/main-webserver.js").webServer
 /*
 * Build out your compoents with componetBuilder
 * bring them into an index.js to modify them and update so all other logic is done here
+*
+* The idea is to build a compoent in its own js page and the manipulation and logic the flow through the system
+* as it releates to the life of that component is done here so that you arnt worried about the componet after it is built
+* and instead are worried about how they change.
 */
 
 
 
-  
+
   login.build(true);
   webServer.build(true,true)
 
