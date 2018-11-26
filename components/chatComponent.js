1e49617de6a77eae59a37a195af41da0f1c974d1
dshrops1 <dustin.t.shropshire@gmail.com>
2018-11-26 11:23:24 -0700

    added chatComponent

---
+++ components/chatComponent.js
@@ -0,0 +1,10 @@
+/*
+* Authors: Dustin shrosphire
+* Notes:
+*/
+
+let comps = require("../componetBuilder");
+
+let chatComponent = new comps.Builder(``,"chatComponent")
+
+module.exports.chatComponent = chatComponent
